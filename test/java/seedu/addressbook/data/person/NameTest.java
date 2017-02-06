7970e123650488fa5ff23ce3b649d4611040dee3
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-02-07 00:15:51 +0800

    Add empty name test

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -9,14 +9,23 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class NameTest {
 
 	private Name name;
+	private Name nullName;
+	private Name emptyName;
 	
 	@Before
 	public void setup() throws IllegalValueException {
 		name = new Name("Uvuvwevwevwe Onyetenyevwe Ugwemuhwem Ossas");
+		nullName = null;
+		emptyName = new Name("");
 	}
 	
 	@Test
 	public void isSimilar_null_returnsFalse() {
 		assertFalse(name.isSimilar(null));
+	}
+	
+	@Test
+	public void isSimilar_empty_returnsFalse() {
+		assertFalse(name.isSimilar(emptyName));
 	}
 }
