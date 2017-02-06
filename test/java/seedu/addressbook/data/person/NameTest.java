a797b7ab620833e67a03d3bae968f3ba52f669b0
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-02-07 00:21:51 +0800

    Add different case name test

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -11,21 +11,34 @@ public class NameTest {
 	private Name name;
 	private Name nullName;
 	private Name emptyName;
+	private Name upperName;
+	private Name lowerName;
+	private Name upperAndLowerName;
 	
 	@Before
 	public void setup() throws IllegalValueException {
 		name = new Name("Uvuvwevwevwe Onyetenyevwe Ugwemuhwem Ossas");
 		nullName = null;
 		emptyName = new Name("");
+		upperName = new Name("Uvuvwevwevwe Onyetenyevwe Ugwemuhwem Ossas".toUpperCase());
+		lowerName = new Name("Uvuvwevwevwe Onyetenyevwe Ugwemuhwem Ossas".toLowerCase());
+		upperAndLowerName = new Name("uvuvwEvwEvwE onyEtEnyEvwE ugwEmuhwEm ossas");
 	}
 	
 	@Test
-	public void isSimilar_null_returnsFalse() {
-		assertFalse(name.isSimilar(null));
+	public void isSimilar_nullName_returnsFalse() {
+		assertFalse(name.isSimilar(nullName));
 	}
 	
 	@Test
-	public void isSimilar_empty_returnsFalse() {
+	public void isSimilar_emptyName_returnsFalse() {
 		assertFalse(name.isSimilar(emptyName));
+	}
+	
+	@Test
+	public void isSimilar_differentCase_returnsTrue() {
+		assertTrue(name.isSimilar(upperName));
+		assertTrue(name.isSimilar(lowerName));
+		assertTrue(name.isSimilar(upperAndLowerName));
 	}
 }
